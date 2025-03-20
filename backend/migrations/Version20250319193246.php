d7524a008cbef5f52285191b62a7a6ae31dffb3e
wergh <carlos.cousillas@gmail.com>
2025-03-20 04:05:30 +0100

    fix: Fix migration

--- backend/migrations/Version20250319193246.php
+++ backend/migrations/Version20250319193246.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250319193246 extends AbstractMigration
+final class Version20250320030216 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,9 +22,9 @@ final class Version20250319193246 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE users (id CHAR(36) NOT NULL COMMENT \'(DC2Type:user_id)\', name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE work_entries (id CHAR(36) NOT NULL COMMENT \'(DC2Type:work_entry_id)\', user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:user_id)\', start_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', end_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_F8330BE7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE work_entry_logs (id INT AUTO_INCREMENT NOT NULL, work_entry_id CHAR(36) NOT NULL COMMENT \'(DC2Type:work_entry_id)\', updated_by_user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:user_id)\', start_time DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', end_time DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_4FCE139C6F83C31E (work_entry_id), INDEX IDX_4FCE139C2793CC5E (updated_by_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE work_entries ADD CONSTRAINT FK_F8330BE7A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE work_entry_logs ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE work_entry_logs ADD CONSTRAINT FK_4FCE139C6F83C31E FOREIGN KEY (work_entry_id) REFERENCES work_entries (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE work_entry_logs ADD CONSTRAINT FK_4FCE139C2793CC5E FOREIGN KEY (updated_by_user_id) REFERENCES users (id) ON DELETE RESTRICT');
     }
@@ -32,12 +32,12 @@ final class Version20250319193246 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE work_entry_logs DROP FOREIGN KEY FK_4FCE139C2793CC5E');
-        $this->addSql('ALTER TABLE work_entry_logs DROP FOREIGN KEY FK_4FCE139C6F83C31E');
         $this->addSql('ALTER TABLE work_entries DROP FOREIGN KEY FK_F8330BE7A76ED395');
+        $this->addSql('ALTER TABLE work_entry_logs DROP FOREIGN KEY FK_4FCE139C6F83C31E');
+        $this->addSql('ALTER TABLE work_entry_logs DROP FOREIGN KEY FK_4FCE139C2793CC5E');
         $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE work_entries');
+        $this->addSql('DROP TABLE work_entry_logs');
         $this->addSql('DROP TABLE messenger_messages');
-        $this->addSql('ALTER TABLE work_entry_logs DROP created_at');
     }
 }
